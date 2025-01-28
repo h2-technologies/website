f7eea33f5945042b3713a50f591aeec8fac1ed79
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-01-27 22:05:16 -0500

    svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<footer class="bg-zinc-800 text-white fixed w-full bottom-0 text-center py-4">
+	<p>&copy; 2024 H2 Technologies. All rights reserved.</p>
+</footer>
