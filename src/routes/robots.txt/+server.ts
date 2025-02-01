20bbd7e75e544cca956516c23d76a1747fd7cf6e
Austin Hadley <admin@austinh.dev>
2025-02-01 09:45:59 -0500

    Robots.txt generated at build time
    Signed-off-by: Austin Hadley <admin@austinh.dev>

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,17 @@
+export const prerender = true;
+
+export async function GET(): Promise<Response> {
+  // prettier-ignore
+  const body = [
+    'User-agent: *',
+    'Allow: /',
+    '',
+    `Sitemap: https://h2technologiesllc.com/sitemap.xml`
+  ].join('\n').trim();
+
+  const headers = {
+    'Content-Type': 'text/plain',
+  };
+
+  return new Response(body, { headers });
+}
