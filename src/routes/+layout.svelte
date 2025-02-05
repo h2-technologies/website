d4b17f33a757de80a0229352d985c1f7ced32538
V01D <ecdthegreat@gmail.com>
2025-02-04 23:34:57 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,5 +24,5 @@
 <footer class="bg-zinc-800 text-white fixed w-full bottom-0 text-center py-4 flex justify-center">
 	<a href="" class="hover:underline px-8">Privacy Policy</a>
 	<p>&copy; 2024 H2 Technologies. All rights reserved.</p>
-	<a href="" class="hover:underline px-8">Commitment message</a>
+	<a href="/commitment" class="hover:underline px-8">Commitment message</a>
 </footer>
