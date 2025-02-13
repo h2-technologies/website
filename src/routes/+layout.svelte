72c2f0ab73e247e929b1e04af76daf1996a56937
Austin Hadley <admin@austinh.dev>
2025-02-13 12:17:04 -0500

    Remove Service page and Contact page
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,9 +13,7 @@
 	<ul class="flex space-x-4 justify-self-end">
 		<li><a href="/" class="hover:underline">Home</a></li>
 		<li><a href="/about" class="hover:underline">About</a></li>
-		<li><a href="/services" class="hover:underline">Services</a></li>
 		<li><a href="/commitment" class="hover:underline">Commitment</a></li>
-		<li><a href="/contact" class="hover:underline">Contact</a></li>
 	</ul>
 	</div>
 </nav>
