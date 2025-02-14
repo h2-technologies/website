85a80d1a0901b275665248071fa8a0c8c384ce05
ManOfTheMask <ecdthegreat@gmail.com>
2025-02-13 23:43:32 -0500

    fixed code banner's size so it place nice with css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </svelte:head>
 
 <div class="bg-base-100 m-0 p-0">
-  <div class="flex px-8 w-full">
-    <img src="code.png">
+  <div class="flex w-full">
+    <img src="code.png" class="w-full max-h-24" alt="code" />
   </div>
   <header class="bg-base-200  py-8 px-0 text-center font-bold">
     <h1>Welcome to H2 Technologies</h1>
