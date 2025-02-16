a0894d6e4602c94286254f2e3c63dae2e67a8825
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-02-15 21:25:02 -0500

    idk

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/state';
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  let activeUrl = $state(page.url.pathname);
+  $effect(() => {
+    activeUrl = page.url.pathname;
+  })
+</script>
+
+<Navbar class="bg-base-300" color="none">
+  <NavBrand href="/" alt="H2 Technologies Logo" class="h-8 inline-block mr-4">
+    <img src="/logo.png" alt="H2 Technologies Logo" class="h-8 inline-block mr-4" />
+  </NavBrand>
+  <NavHamburger />
+  <NavUl {activeUrl}>
+    <NavLi class="text-lg font-roboto" href="/">Home</NavLi>
+    <NavLi class="text-lg font-roboto" href="/about">About</NavLi>
+    <NavLi class="text-lg font-roboto" href="/commitment">Commitment</NavLi>
+  </NavUl>
+
+</Navbar>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,53 +1,66 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
+
 <svelte:head>
-    <title>H2 Technologies</title>
+  <title>H2 Technologies</title>
 </svelte:head>
 
-
-<header class="bg-base-200 px-0 py-8 text-center font-bold ">
+<header class="px-0 py-8 text-center text-cyan-700 font-bold ">
 	<h1>About Us</h1>
 </header>
-
-<!--This is a placeholder image replace with a static image eventually-->
-<div class="flex px-8">
-	<img src="doingbusnessstock.png" alt="About Us" class="m-auto h-1/3 w-2/5" />
-	<img src="teamworkstock.png" alt="About Us" class="m-auto h-1/3 w-2/5" />
-</div>
 <div class="p-4">
-	<div class="p-8 bg-base-200 rounded-xl">
-		<h2>About Us</h2>
-		<div class="rounded-xl bg-base-300 p-8 shadow-lg">	
-			<p>
-				H2 Technologies is a leading provider of innovative tech solutions. We specialize in
-				software development, IT consulting, and cutting-edge technology services.
-			</p>
-			<h2 class="pt-1">Our Services</h2>
-			<ul class="ml-5 block list-disc">
-				<li class="ml-5">Software Development</li>
-				<li class="ml-5">IT Consulting</li>
-			</ul>
-			<h2 class="pt-1">Contact Us</h2>
-			<p>
-				Email: <a href="mailto:sales@h2technologiesllc.com" class="text-blue-500 underline"
-					>info@h2technologiesllc.com</a
-				>
-			</p>
-			<p>Phone: <a href="tel:5672610762" class="text-blue-500 underline">(567) 261-0762</a></p>
-		</div>
-	</div>
-</div>
-
-<div class="p-4 mb-2">
-	<div class="p-8 bg-base-200 rounded-xl">
-		<h2 class="mb-4 text-2xl font-bold">Our Mission</h2>
-		<div class="rounded-xl bg-base-300 p-8 shadow-lg">
-			<p>
-				At H2 Technologies, our mission is to empower organizations through adaptable,
-				forward-thinking technology solutions that foster sustainable growth and drive long-term
-				success. We are committed to delivering personalized service, ensuring the highest level of
-				integrity, and maintaining unwavering attention to detail in everything we do. By
-				cultivating a culture of continuous innovation and collaboration, we aim to become a trusted
-				partner—enabling clients to confidently embrace the digital future
-			</p>
+	<div class="p-8 rounded-xl">
+		<h2 class="mb-4 text-2xl text-cyan-700 font-bold text-center">Who We Are</h2>
+		<div class="flex flex-wrap justify-center">
+			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-3 w-64 outline-cyan-700">
+				<img src="/austin.jpg" alt="Austin Hadley" class="w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
+				<div class="my-5">
+					<h3 class="text-2xl font-bold">Austin Hadley</h3>
+					<p class="text-xl">Chief Executive Officer</p>
+					<p class="text-sm">Member since 2023</p>
+				</div>
+				<div class="flex justify-center mt-2">
+					<a href="mailto:austinhadley@h2technologiesllc.com" class="outline rounded-full p-1 mr-2 outline-cyan-700">
+						<Icon icon="mdi:email" class="text-2xl text-cyan-700"/>
+					</a>
+					<a href="tel:5672610762,21610" class="outline rounded-full p-1 outline-cyan-700">
+						<Icon icon="mdi:phone" class="text-2xl text-cyan-700"/>
+					</a>
+				</div>
+			</div>
+			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-3 w-64 outline-cyan-700">
+				<img src="/samuel.jpg" alt="Austin Hadley" class="w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
+				<div class="my-5">
+					<h3 class="text-2xl font-bold">Samuel Valencia</h3>
+					<p class="text-xl ">Chief Operating Officer</p>
+					<p class="text-sm">Member since 2023</p>
+				</div>
+				<div class="flex justify-center mt-2">
+					<a href="mailto:samuelvalencia@h2technologiesllc.com" class="outline rounded-full p-1 mr-2 outline-cyan-700">
+						<Icon icon="mdi:email" class="text-2xl text-cyan-700"/>
+					</a>
+					<a href="tel:5672610762,21611" class="outline rounded-full p-1 outline-cyan-700">
+						<Icon icon="mdi:phone" class="text-2xl text-cyan-700"/>
+					</a>
+				</div>
+			</div>
+			<div class="text-center text-amber-600 outline rounded-lg p-5 w-64 outline-cyan-700">
+				<Icon icon="mdi:person" class="text-slate-800 w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
+				<div class="my-5">
+					<h3 class="text-2xl font-bold">Eric Doepke</h3>
+					<p class="text-xl ">Junior Developer</p>
+					<p class="text-sm">Member since 2025</p>
+				</div>
+				<div class="flex justify-center mt-2">
+					<a href="mailto:ericdoepke@h2technologiesllc.com" class="outline rounded-full p-1 mr-2 outline-cyan-700">
+						<Icon icon="mdi:email" class="text-2xl text-cyan-700"/>
+					</a>
+					<a href="tel:5672610762,21612" class="outline rounded-full p-1 outline-cyan-700">
+						<Icon icon="mdi:phone" class="text-2xl text-cyan-700"/>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
