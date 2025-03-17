eda51338b84569699038f647b94ba0cf02fac9ef
JoshAviation <joshuafromnhplays@gmail.com>
2025-03-17 19:29:26 -0400

    Made minor visual changes
    Visual Changes including color and text size.

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -7,16 +7,17 @@
   })
 </script>
 
-<Navbar class="bg-base-300" color="none">
-  <NavBrand href="/" alt="H2 Technologies Logo" class="h-8 inline-block mr-4">
-    <img src="/logo.png" alt="H2 Technologies Logo" class="h-8 inline-block mr-4" />
+<Navbar class="bg-slate-800 rounded-b-lg shadow-lg" color="none">
+  <NavBrand href="/" alt="H2 Technologies Logo" class=" inline-block mr-4">
+    <img src="/whitelogo.png" alt="H2 Technologies Logo" class="h-16 inline-block" />
+    <h1 class="text-2xl inline-flex align-middle text-neutral-300">Technologies</h1>
   </NavBrand>
   <NavHamburger />
   <NavUl {activeUrl}>
-    <NavLi class="text-lg font-roboto" href="/">Home</NavLi>
-    <NavLi class="text-lg font-roboto" href="/about">About</NavLi>
-    <NavLi class="text-lg font-roboto" href="/commitment">Commitment</NavLi>
-    <NavLi class="text-lg font-roboto" href="/contact">Contact</NavLi>
+    <NavLi class="text-2xl font-nunito" href="/">Home</NavLi>
+    <NavLi class="text-2xl font-nunito" href="/about">About</NavLi>
+    <NavLi class="text-2xl font-nunito" href="/commitment">Commitment</NavLi>
+    <NavLi class="text-2xl font-nunito" href="/contact">Contact</NavLi>
   </NavUl>
 
 </Navbar>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,11 +9,11 @@
 <header class="px-0 py-8 text-center text-cyan-700 font-bold ">
 	<h1>About Us</h1>
 </header>
-<div class="p-4">
-	<div class="p-8 rounded-xl">
+<div class="p-2">
+	<div class="p-9 rounded-xl">
 		<h2 class="mb-4 text-2xl text-cyan-700 font-bold text-center">Who We Are</h2>
 		<div class="flex flex-wrap justify-center">
-			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-3 w-64 outline-cyan-700">
+			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-8 w-64 outline-cyan-700">
 				<img src="/austin.jpg" alt="Austin Hadley" class="w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
 				<div class="my-5">
 					<h3 class="text-2xl font-bold">Austin Hadley</h3>
@@ -29,7 +29,7 @@
 					</a>
 				</div>
 			</div>
-			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-3 w-64 outline-cyan-700">
+			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-8 w-64 outline-cyan-700">
 				<img src="/samuel.jpg" alt="Austin Hadley" class="w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
 				<div class="my-5">
 					<h3 class="text-2xl font-bold">Samuel Valencia</h3>
@@ -45,7 +45,7 @@
 					</a>
 				</div>
 			</div>
-			<div class="text-center text-amber-600 outline rounded-lg p-5 w-64 outline-cyan-700">
+			<div class="text-center text-amber-600 outline rounded-lg p-5 mr-8 w-64 outline-cyan-700">
 				<Icon icon="mdi:person" class="text-slate-800 w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
 				<div class="my-5">
 					<h3 class="text-2xl font-bold">Eric Doepke</h3>
@@ -59,6 +59,22 @@
 					<a href="tel:5672610762,21612" class="outline rounded-full p-1 outline-cyan-700">
 						<Icon icon="mdi:phone" class="text-2xl text-cyan-700"/>
 					</a>
+				</div>
+			</div>
+			<div class="text-center text-amber-600 outline rounded-lg p-5 w-64 outline-cyan-700">
+				<Icon icon="mdi:person" class="text-slate-800 w-24 h-24 mx-auto rounded-full mb-1 outline outline-cyan-700"/>
+				<div class="my-5">
+					<h3 class="text-2xl font-bold">Joshua Lambert</h3>
+					<p class="text-xl ">Junior Graphics Designer</p>
+					<p class="text-sm">Member since 2025</p>
+				</div>
+				<div class="flex justify-center mt-2">
+					<a href="mailto:joshualambert@h2technologiesllc.com" class="outline rounded-full p-1 outline-cyan-700">
+						<Icon icon="mdi:email" class="text-2xl text-cyan-700"/>
+					</a>
+					<!-- <a href="tel:5672610762,21612" class="outline rounded-full p-1 outline-cyan-700">
+						<Icon icon="mdi:phone" class="text-2xl text-cyan-700"/>
+					</a> ADD LATER WHEN RECIEVED PHONE EXT ALSO ADD BACK MR-2 IN EMAIL-->
 				</div>
 			</div>
 		</div>
