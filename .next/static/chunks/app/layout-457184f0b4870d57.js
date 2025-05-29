fe37370cb7ec9f997b321fc31cb256c63358aa6e
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-05-29 13:34:07 -0400

    fix gitignore

--- .next/static/chunks/app/layout-457184f0b4870d57.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{528:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1685,23)),Promise.resolve().then(n.t.bind(n,6378,23)),Promise.resolve().then(n.t.bind(n,5297,23))},5297:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[610,190,309,951,358],()=>s(528)),_N_E=e.O()}]);
