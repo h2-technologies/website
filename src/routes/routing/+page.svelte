7e5808849983e18ef4390e6b7a5e238c966331dd
ahadley1124 <admin@austinh.dev>
2025-10-11 20:07:42 -0400

    Add Peering Policy page for MANRS Registration
    Signed-off-by: ahadley1124 <admin@austinh.dev>

---
+++ src/routes/routing/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { PageProps } from './$types';
+
+    let { data }: PageProps = $props();
+</script>
+
+<iframe
+    src="/bgp-routing-policy.pdf" 
+    width="100%" 
+    height="600px" 
+    style="border: none;"
+    title="BGP Routing Policy AS-17290">
+</iframe>
